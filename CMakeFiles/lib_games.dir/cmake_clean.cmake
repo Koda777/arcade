79fc8ccf11f5ee835cddf871955d573c97181c27
Koda <alexandre.decobert@epitech.eu>
2023-12-15 14:19:04 +0100

    [DELETE] useless files

--- CMakeFiles/lib_games.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lib_games"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lib_games.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
